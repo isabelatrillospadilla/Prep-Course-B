eecb01a8e4d889551d09dee780c1e60b060ea347
isabelatrillospadilla <isabelatrillospadilla@gmail.com>
2022-11-28 20:38:07 -0300

    hw JIII terminada

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -14,43 +14,70 @@ function imprimirSumaNumeros() {
     return acc + elemento;
 });
  return suma;
+
+ /*
+ var suma = 0 
+ for (var i = 0; i < 11; i++)
+ suma = suma + i
+ return suma;
+ */
+ 
+ /*
+ var suma = 0;
+ var i = 1;
+ while (i < 11){
+  suma = suma + i;
+  i++
+ }
+ return suma;
+ */
+
+ /*
+
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var suma = 0;
+for (var element of numeros){
+  suma = elemetn + suma; 
+}
+return suma;
+*/
+
+/*var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var suma = 0;
+numeros.forEach((element)  => {
+  suma = suma + element;
+});
+return suma;
+*/
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+   return array.filter((element) => element % 2 === 0)
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-  var numeros = [1, 2, 3 ,5, 8, 9, 11 ,14,20];
-  var doble = numeros.map(function (elemento){
-  return elemento *2});
-  doble
-    
+  var cuadrado = array.map ((element) =>  element ** 2);
+  return cuadrado;
+  /* return array.map((element) => element ** 2)*/  
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
-  var numeros = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, ];
-
-    var suma = numeros.reduce(function (acc, elemento) {
-    return acc + elemento;
-});
- return suma;
-  
+  return array.reduce((acc , sgte) => acc + sgte);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
-  var longitud = num.length;
-  return longitud;
+  return num.toString().length;
 }
   
   
